49997eeb89f06b46accab795d10beae8646641b8
angel <angelsmithlgs@gmail.com>
2024-10-08 10:50:53 -0300

    build: reestructura de portfolio de tsc a js

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {
+  About,
+  Experience,
+  Footer,
+  Hero,
+  Navbar,
+  Projects,
+} from "./components";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
+
