d915148a90c3a6bcad0842ded023a8b9aeefeae9
angel <angelsmithlgs@gmail.com>
2024-10-14 17:51:28 -0300

    feat: se añaden mas utilidades

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         generalsans: ['General Sans', 'sans-serif'],
+        gotham: ['GothamBold', 'sans-serif'],
+        gothamBook: ['GothamBook', 'sans-serif'],
+        gothamMedium: ['GothamMedium', 'sans-serif'],
       },
       colors: {
         black: {
@@ -22,6 +24,18 @@ export default {
           600: '#AFB0B6',
           500: '#62646C',
         },
+        purple: {
+          DEFAULT: '#8F00FF', 
+          100: '#F3E5FF',      
+          200: '#D1B3FF',      
+          300: '#AF80FF',  
+          400: '#8F4DFF', 
+          500: '#8F00FF', 
+          600: '#7500CC', 
+          700: '#5B0099', 
+          800: '#400066', 
+          900: '#260033', 
+        }
       },
       backgroundImage: {
         terminal: "url('/assets/terminal.png')",
