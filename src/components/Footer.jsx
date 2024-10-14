bbd419bd90f75b0d07b69527c9f30049f379e959
angel <angelsmithlgs@gmail.com>
2024-10-14 17:49:58 -0300

    feat: se añade footer con texto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-center items-center flex-wrap gap-5">
+      <p className="text-white-500 text-center">V N G © 2024 Powered by 🐈</p>
+    </footer>
+  );
 };
 
 export default Footer;
