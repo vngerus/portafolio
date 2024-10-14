db247cff1d14ce2c0ae822f4cae2420306a831b4
angel <angelsmithlgs@gmail.com>
2024-10-14 17:50:17 -0300

    styles: se ajustan textos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,29 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-import { useMediaQuery } from "react-responsive";
-import { calculateResponsiveSizes } from "../constant";
-import CanvasLoader from "../tools/CanvasLoader";
-import BananaCatHi from "../model/BananaCat";
+import { PerspectiveCamera } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { calculateResponsiveSizes } from '../constant';
+import CanvasLoader from '../tools/CanvasLoader';
+import BananaCatHi from '../model/BananaCat';
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-  const deviceType = isMobile ? "mobile" : isTablet ? "tablet" : "desktop";
-  const { catScale, catPosition, cameraPosition } =
-    calculateResponsiveSizes(deviceType);
+  const deviceType = isMobile ? 'mobile' : isTablet ? 'tablet' : 'desktop';
+  const { catScale, catPosition, cameraPosition } = calculateResponsiveSizes(deviceType);
 
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col mt-20 c-space gap-2">
-        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+        <p className="sm:text-xl text-2xl font-medium text-white text-center hero_tag text-gray_gradient">
           Hola, Me llamo Ángel <span className="waving-hand">✌</span>
         </p>
         <p className="hero_tag text-gray_gradient"> Front-end Developer</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
-        <Canvas style={{ width: "100%", height: "100%" }}>
+        <Canvas style={{ width: '100%', height: '100%' }}>
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={cameraPosition} />
             <BananaCatHi
