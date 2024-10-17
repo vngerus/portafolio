588baa5a40357e7dc1590e94e8cc6c216b2a8680
angel <angelsmithlgs@gmail.com>
2024-10-17 12:57:14 -0300

    styles: se ajusta la funcion de estilos de la pagina hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
@@ -13,13 +15,58 @@ const Hero = () => {
   const deviceType = isMobile ? 'mobile' : isTablet ? 'tablet' : 'desktop';
   const { catScale, catPosition, cameraPosition } = calculateResponsiveSizes(deviceType);
 
+  const [developerType, setDeveloperType] = useState('Full Stack');
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setDeveloperType((prevType) => (prevType === 'Full Stack' ? 'Front-end' : 'Full Stack'));
+      }, 800);
+
+      setTimeout(() => {
+        setIsTransitioning(false);
+      }, 1600);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <section className="min-h-screen w-full flex flex-col relative">
-      <div className="w-full mx-auto flex flex-col mt-20 c-space gap-2">
-        <p className="sm:text-xl text-2xl font-medium text-white text-center hero_tag text-gray_gradient">
-          Hola, Me llamo Ángel <span className="waving-hand">✌</span>
-        </p>
-        <p className="hero_tag text-gray_gradient"> Front-end Developer</p>
+    <section className="min-h-screen w-full flex flex-col justify-center items-center relative">
+      <div className="w-full flex flex-col items-start text-left z-10 mt-[-300px]">
+        <div className="text-xl uppercase text-gray-400 ml-[25%]">Angel Smith</div>
+
+        <div className="text-6xl sm:text-8xl font-medium text-white leading-tight relative inline-block ml-[25%] mt-2">
+          <div className="relative inline-block overflow-hidden">
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={developerType}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.8 }}
+                className="inline-block relative z-20">
+                <span className="text-purple-500">+</span> {developerType}
+              </motion.div>
+            </AnimatePresence>
+          </div>
+
+          <motion.div
+            className="absolute bottom-0 left-0 h-full bg-purple-500 z-50"
+            style={{ width: `${developerType === 'Full Stack' ? 11 : 10}ch` }}
+            initial={{ width: 0 }}
+            animate={{ width: isTransitioning ? `${developerType === 'Full Stack' ? 11 : 10}ch` : 0 }}
+            exit={{ width: 0 }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+          />
+        </div>
+
+        <div className="flex items-center justify-start mt-4 ml-[25%]">
+          <div className="h-1 w-32 bg-purple-500"></div>
+          <h1 className="text-6xl sm:text-8xl font-medium text-white leading-tight ml-4">Developer</h1>
+        </div>
       </div>
 
       <div className="w-full h-full absolute inset-0">
