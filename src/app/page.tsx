f38e977f01d5559abd87bae6a7fe9b306c33bc7c
angel <angelsmithlgs@gmail.com>
2024-11-20 17:01:46 -0300

    styles: navbar up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Header, Hero, About, Projects, Experiences, Contact, Footer } from '@/s
 
 export default function Home() {
   return (
-    <div className=" min-h-screen text-white">
+    <div className=" min-h-screen text-white bg-[#0A0825]
+">
       <Header />
       <Hero />
       <About />
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,28 +1,83 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState } from 'react';
-import { FaMoon, FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <nav className="bg-black text-white fixed w-full top-0 z-50">
-            <div className="flex items-center justify-between px-6 md:px-12 py-4 max-w-screen-xl mx-auto">
-                <div className="flex items-center space-x-4">
-                    <Image
-                        src="/logos/gato.avif"
-                        width={40}
-                        height={40}
-                        alt="Vngerus Logo"
-                    />
-                    <span className="text-xl font-bold tracking-wide">Vngerus</span>
+        <nav
+            className={`fixed w-full top-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-[#0A0825]/80 backdrop-blur-md' : 'bg-[#0A0825]'
+                } text-white`}
+        >
+            <div className="flex items-center justify-between px-6 py-4 max-w-screen-xl mx-auto">
+                <div className="flex items-center space-x-6">
+                    <div className="flex items-center space-x-4">
+                        <Image
+                            src="/logos/gato.avif"
+                            width={40}
+                            height={40}
+                            alt="Vngerus Logo"
+                        />
+                        <span className="text-xl font-bold tracking-wide uppercase">
+                            @Vngerus
+                        </span>
+                    </div>
+
+                    <div className="hidden md:flex items-center space-x-2 font-semibold">
+                        <a
+                            href="https://www.linkedin.com/in/angelsmithl/"
+                            className="hover:text-[#D28FFF] transition-all duration-300 ease-in-out text-xs uppercase"
+                        >
+                            LinkedIn
+                        </a>
+                        <span className="text-gray-400">/</span>
+                        <a
+                            href="https://github.com/vngerus"
+                            className="hover:text-[#D28FFF] transition-all duration-300 ease-in-out text-xs uppercase"
+                        >
+                            Github
+                        </a>
+                    </div>
                 </div>
+
+                <ul className="hidden md:flex items-center space-x-2 text-xs font-semibold uppercase">
+                    {[{ href: '#projects', label: 'Proyectos' },
+                    { href: '#aboutme', label: 'Sobre Mi' },
+                    { href: '#contact', label: 'Contacto' }].map((item, index) => (
+                        <li key={index} className="flex items-center">
+                            <a
+                                href={item.href}
+                                className="hover:text-[#D28FFF] transition-all duration-300 ease-in-out"
+                            >
+                                {item.label}
+                            </a>
+                            {index < 2 && (
+                                <span className="mx-4 text-gray-400">/</span>
+                            )}
+                        </li>
+                    ))}
+                </ul>
 
                 <button
                     onClick={toggleMenu}
@@ -30,61 +85,18 @@ const Header: React.FC = () => {
                 >
                     {isOpen ? <FaTimes /> : <FaBars />}
                 </button>
-
-                <div className="hidden md:flex items-center space-x-6">
-                    <a
-                        href="https://github.com"
-                        className="hover:text-purple-500 transition-all duration-300 ease-in-out text-sm uppercase"
-                    >
-                        GitHub
-                    </a>
-                    <span className="text-gray-400">/</span>
-                    <a
-                        href="https://reddit.com"
-                        className="hover:text-purple-500 transition-all duration-300 ease-in-out text-sm uppercase"
-                    >
-                        Reddit
-                    </a>
-                </div>
-
-                <ul className="hidden md:flex items-center space-x-8 text-sm font-medium uppercase">
-                    {[
-                        { href: '#home', label: 'Home' },
-                        { href: '#projects', label: 'Projects' },
-                        { href: '#articles', label: 'Articles' },
-                        { href: '#contact', label: 'Contact' },
-                    ].map((item, index) => (
-                        <li key={index} className="flex items-center">
-                            <a
-                                href={item.href}
-                                className="hover:text-purple-500 transition-all duration-300 ease-in-out"
-                            >
-                                {item.label}
-                            </a>
-                            {index < 3 && (
-                                <span className="mx-2 text-gray-400">/</span>
-                            )}
-                        </li>
-                    ))}
-                    <li>
-                        <button className="hover:text-purple-500 transition-all duration-300 ease-in-out">
-                            <FaMoon />
-                        </button>
-                    </li>
-                </ul>
             </div>
 
             {isOpen && (
-                <div className="md:hidden bg-black text-white w-full h-screen fixed top-0 left-0 z-40">
+                <div className="md:hidden bg-[#0A0825] text-white w-full h-screen fixed top-0 left-0 z-40">
                     <div className="flex items-center justify-between px-6 py-4">
                         <div className="flex items-center space-x-4">
                             <Image
                                 src="/logos/gato.avif"
                                 width={40}
                                 height={40}
-                                alt="RUTKULI Logo"
+                                alt="Vngerus Logo"
                             />
-                            <span className="text-xl font-bold tracking-wide">Vngerus</span>
                         </div>
 
                         <button
@@ -96,16 +108,13 @@ const Header: React.FC = () => {
                     </div>
 
                     <div className="flex flex-col items-center space-y-8 mt-8 text-lg">
-                        {[
-                            { href: '#home', label: 'Home' },
-                            { href: '#projects', label: 'Projects' },
-                            { href: '#articles', label: 'Articles' },
-                            { href: '#contact', label: 'Contact' },
-                        ].map((item, index) => (
+                        {[{ href: '#projects', label: 'Proyectos' },
+                        { href: '#aboutme', label: 'Sobre Mi' },
+                        { href: '#contact', label: 'Contacto' }].map((item, index) => (
                             <a
                                 key={index}
                                 href={item.href}
-                                className="hover:text-purple-500 transition-all duration-300 ease-in-out"
+                                className="hover:text-[#D28FFF] transition-all duration-300 ease-in-out"
                                 onClick={() => setIsOpen(false)}
                             >
                                 {item.label}
