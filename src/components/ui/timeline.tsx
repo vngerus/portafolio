7d076ce80434f973bf48a5a556e7c1eb75de3e54
angel <angelsmithlgs@gmail.com>
2024-11-21 18:22:57 -0300

    feat: timeline, exp, experienice general

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -33,18 +33,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
     return (
         <div
-            className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
+            className="w-full font-sans md:px-10"
             ref={containerRef}
         >
-            <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-                <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-                    Changelog from my journey
-                </h2>
-                <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-                    I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-                    a timeline of my journey.
-                </p>
-            </div>
+
 
             <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
                 {data.map((item, index) => (
@@ -53,16 +45,16 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
                         className="flex justify-start pt-10 md:pt-40 md:gap-10"
                     >
                         <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-                            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
-                                <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+                            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full  flex items-center justify-center">
+                                <div className="h-4 w-4 rounded-full bg-neutral-200 border border-neutral-300 p-2" />
                             </div>
-                            <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+                            <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500  ">
                                 {item.title}
                             </h3>
                         </div>
 
                         <div className="relative pl-20 pr-4 md:pl-4 w-full">
-                            <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+                            <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500">
                                 {item.title}
                             </h3>
                             {item.content}{" "}
@@ -73,7 +65,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
                     style={{
                         height: height + "px",
                     }}
-                    className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+                    className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
                 >
                     <motion.div
                         style={{
--- src/sections/Experiences.tsx
+++ src/sections/Experiences.tsx
@@ -1,12 +1,77 @@
 "use client"
-import React from 'react';
 
-const Experiences: React.FC = () => {
+import React, { useEffect, useRef, useState } from 'react';
+import { useScroll, useTransform, MotionValue } from 'framer-motion';
+import { Exp } from '@/components';
+import { certifications, education, workExperience } from '@/constant';
+
+type Category = 'work' | 'education' | 'certifications';
+
+const Experience: React.FC = () => {
+    const [selectedCategory, setSelectedCategory] = useState<Category>('work');
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [height, setHeight] = useState<number>(0);
+
+    const categories = {
+        work: workExperience,
+        education: education,
+        certifications: certifications,
+    };
+
+    const data = categories[selectedCategory] || [];
+
+    useEffect(() => {
+        if (containerRef.current) {
+            const rect = containerRef.current.getBoundingClientRect();
+            setHeight(rect.height);
+        }
+    }, [selectedCategory]);
+
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ['start 10%', 'end 50%'],
+    });
+
+    const heightTransform: MotionValue<number> = useTransform(scrollYProgress, [0, 1], [0, height]);
+    const opacityTransform: MotionValue<number> = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+
     return (
-        <div>
-            Experiences
+        <div className="z-0 mx-auto max-w-screen-lg px-4 xs:px-4 sm:px-6 md:px-8" ref={containerRef}>
+            <div className="flex justify-center space-x-8 mb-6">
+                {Object.keys(categories).map((key) => (
+                    <button
+                        key={key}
+                        onClick={() => setSelectedCategory(key as Category)}
+                        className={`text-lg ${selectedCategory === key ? 'text-purple-500' : 'text-gray-500'
+                            }`}>
+                        {key === 'education' && (
+                            <span role="img" aria-label="Education">
+                                🎓
+                            </span>
+                        )}
+                        {key === 'work' && (
+                            <span role="img" aria-label="Work">
+                                💼
+                            </span>
+                        )}
+                        {key === 'certifications' && (
+                            <span role="img" aria-label="Certifications">
+                                📜
+                            </span>
+                        )}
+                        {` ${key.charAt(0).toUpperCase() + key.slice(1)}`}
+                    </button>
+                ))}
+            </div>
+
+            <Exp
+                data={data}
+                heightTransform={heightTransform}
+                opacityTransform={opacityTransform}
+                height={height}
+            />
         </div>
     );
 };
 
-export default Experiences;
+export default Experience;
