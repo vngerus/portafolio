be30a9fcad0aca44cfd75c29e713688991fb4f6a
angel <angelsmithlgs@gmail.com>
2024-12-10 18:11:26 -0300

    feat: index de data para textos

---
+++ src/data/index.ts
@@ -0,0 +1,32 @@
+export const experiences = [
+    {
+        company: 'CIB',
+        role: 'Front-End Developer',
+        duration: 'Mar 2023 - Presente',
+        description: [
+            'Desarrollé interfaces de usuario utilizando React y Tailwind CSS.',
+            'Optimicé la experiencia de usuario en aplicaciones web.',
+            'Colaboré con equipos para implementar nuevas funcionalidades.',
+        ],
+    },
+    {
+        company: 'SEDSA',
+        role: 'Developer Intern',
+        duration: 'Ene 2022 - Feb 2023',
+        description: [
+            'Construí prototipos en Figma para aplicaciones web.',
+            'Desarrollé aplicaciones web con React, TypeScript y FastAPI.',
+            'Participé en revisiones de código y reuniones de planificación.',
+        ],
+    },
+    {
+        company: 'Monkey Agency',
+        role: 'Consultor',
+        duration: 'Mar 2018 - Dic 2021',
+        description: [
+            'Analicé datos de mercado para mejorar estrategias de marketing.',
+            'Implementé campañas de SEO y SEM efectivas.',
+            'Gestioné proyectos digitales en múltiples plataformas.',
+        ],
+    },
+];
