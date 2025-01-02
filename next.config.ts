eb00813ba0abdb5a03ef4a5b431e1896fe03449c
angel <angelsmithlgs@gmail.com>
2025-01-02 00:40:48 -0300

    rollback

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glb|gltf|hdr|png|jpg|jpeg|svg|mp4)$/,
-      type: "asset/resource",
-    });
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
